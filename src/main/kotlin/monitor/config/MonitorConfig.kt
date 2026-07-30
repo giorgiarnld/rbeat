57e910397045f37011d4ae808fff84012d10eefe
Giorgia <giorgiarnld@proton.me>
2026-07-30 19:23:36 +0300

    Create MonitorConfig.kt

---
+++ src/main/kotlin/monitor/config/MonitorConfig.kt
@@ -0,0 +1,14 @@
+package monitor.config
+
+object MonitorConfig {
+
+    const val MAX_LATENCY = 150
+
+    const val MIN_SIGNAL = 60
+
+    const val REFRESH_INTERVAL = 10
+
+    const val DEFAULT_LOCATION =
+        "Main Office"
+
+}
