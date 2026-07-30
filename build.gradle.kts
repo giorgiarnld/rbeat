050c771fe44bd575d0ec142cd434f4f7478ba8f4
Giorgia <giorgiarnld@proton.me>
2026-07-30 19:22:36 +0300

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("jvm") version "2.1.0"
+}
+
+group = "com.monitor"
+
+version = "1.0.0"
+
+repositories {
+
+    mavenCentral()
+
+}
+
+dependencies {
+
+    testImplementation(kotlin("test"))
+
+}
+
+tasks.test {
+
+    useJUnitPlatform()
+
+}
+
+kotlin {
+
+    jvmToolchain(17)
+
+}
